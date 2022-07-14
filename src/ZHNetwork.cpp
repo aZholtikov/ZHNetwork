e81bbb9758230a0a42a82fbe56f58370c4b509c9
aZholtikov <github@zh.com.ru>
2022-07-14 11:43:47 +0300

    Незначительные изменения

--- src/ZHNetwork.cpp
+++ src/ZHNetwork.cpp
@@ -278,7 +278,7 @@ void ZHNetwork::maintenance()
             }
         }
     }
-    if (!queueForOutgoingData.empty() && ((millis() - lastMessageSentTime) > (maxWaitingTimeBetweenTransmissions + random(20))))
+    if (!queueForOutgoingData.empty() && ((millis() - lastMessageSentTime) > (maxWaitingTimeBetweenTransmissions + byte(random(20)))))
     {
         OutgoingData outgoingData = queueForOutgoingData.front();
         esp_now_send(outgoingData.intermediateTargetMAC, (byte *)&outgoingData.transmittedData, sizeof(TransmittedData));
